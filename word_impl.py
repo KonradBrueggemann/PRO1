c864fc54c90b85417bef28ea657c619b95352e7f
Julia Mühlbruch <julia.muehlbruch@uni-potsdam.de>
2021-11-28 17:58:36 +0100

    Das Übungsblatt wird abgegeben.

--- word_impl.py
+++ word_impl.py
@@ -1,40 +1,107 @@
+from Histo_1 import histogramm
+# Aufgabe 1 - Magic Methods
 class Word:
-    string_representation = "birne"
+    string_representation = ""
 
-    def number_of_characters(word_input):
-        x = len(word_input)
-        return f"Das Wort '{word_input}' enthält {x} Zeichen."
-    print(number_of_characters(string_representation))
+    def __init__(self, string):
+        """
+        Set-Operation. Initializes an
+        instance of the Word class taking
+        a string argumenr for the internal
+        strin_representation state.
+        """
+        self.string_representation = string
 
-    def capitalize(word_input):
-        return word_input.capitalize()
-    print(capitalize(string_representation))
+    def number_of_characters(self):
+        """
+        Get-Operation. Returns a string containing
+        the value of string_representation and the
+        number of letters in it.
+        """
+        return "Das Wort '{}' enthält {} Zeichen." \
+            .format(self.string_representation, len(self.string_representation))
 
-    def get_ascii(word_input):
-        ascii_value = ""
-        for symbol in word_input:
-            ascii_value += str(ord(symbol)) + " "
-        return ascii_value
-    print(get_ascii(string_representation))
+    def capitalize(self):
+        """
+        Get-Operation. Returns a string containing
+        the capitalized version
+        of the string_representation.
+         """
+        return self.string_representation.title()
 
-    def histogramm(input_sentence, special_characters=False, count_all=True):
-        sentence = input_sentence.lower()
-        dict = {}
-        if special_characters:
-            sentence = sentence.replace("ö", "oe")
-            sentence = sentence.replace("ü", "ue")
-            sentence = sentence.replace("ä", "ae")
-            sentence = sentence.replace("ß", "ss")
-        if count_all:
-            alphabet = "abcdefghijklmnopqrstuvwxyz "
-            for letter in alphabet:
-                dict[letter] = 0
-        for n in sentence:
-            keys = dict.keys()
-            if n in keys:
-                dict[n] += 1
-            else:
-                dict[n] = 1
-        return dict
+    def get_ascii(self):
+        """
+        Get-Operation. Returns a string containing
+        the ASCII version of string_representation.
+        """
+        string_ascii = []
+        for letter in self.string_representation:
+            string_ascii.append(str(ord(letter)))
+        return " ".join(string_ascii)
 
-    print(histogramm(string_representation))
+    def histrogram(self, ):
+        """
+        Get-Operation. Returns a dictionary
+        containing a histogramm for the
+        letter frequency in string_representation.
+        """
+        return histogramm(self.string_representation)
+
+    def __str__(self):
+        """
+        No conditions given.
+        """
+        return str(self.string_representation)
+    def __len__(self):
+        """
+
+        Precondition:
+        Object cant be an integer or boolean
+        """
+        return len(self.string_representation)
+    def __add__(self, other):
+        """
+        Precondition:
+        Both objects have to be a string.
+        """
+        assert type(self) == Word
+        return str(self.string_representation+" "+other)
+    def __reversed__(self):
+        """
+
+        Precondition:
+        has to be string or a list.
+        """
+        return self.string_representation[::-1]
+
+
+w1 = Word("blume")
+w2 = Word("Sonnenfinsternis")
+
+
+print(w1.number_of_characters())
+print(w1.capitalize())
+print(w1.get_ascii())
+print(w1.histrogram())
+
+print(w2.number_of_characters())
+print(w2.capitalize())
+print(w2.get_ascii())
+print(w2.histrogram())
+print(w2.__str__())
+print(w2.__len__())
+print(w2.__add__(w1.__str__()))
+print(w2.__reversed__())
+
+#Aufgabe 2
+def test_cases():
+    assert w1.__str__() == "blume"
+    assert w2.__str__() == "Sonnenfinsternis"
+    assert w1.__len__() == 5
+    assert w2.__len__() == 16
+    assert w1.__add__(w2.__str__()) == "blume Sonnenfinsternis"
+    assert w2.__add__(w1.__str__()) == "Sonnenfinsternis blume"
+    assert w1.__reversed__() == "emulb"
+    assert w2.__reversed__() == "sinretsnifnennoS"
+if __name__ == "__main__":
+    test_cases()
